dfadecde6a32e694dfee878919f1a15df0c5d29b
Taistois mndala <mndalataistois@gmail.com>
2025-09-09 13:27:13 +0200

    Update DB env variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,16 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // required for Railway
   },
 });
 
